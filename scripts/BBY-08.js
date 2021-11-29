c4af03ffa02f71fc38820a67225b07ebec8b893c
CharminJung <90715742+CharminJung@users.noreply.github.com>
2021-11-28 22:23:11 -0800

    Update BBY-08.js

--- scripts/BBY-08.js
+++ scripts/BBY-08.js
@@ -8,6 +8,6 @@ const firebaseConfig = {
   };
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const app = firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
     
